a0a75e19801e1a4d6a9b10d04c889bf2fad6ee8c
Ilya Gyrdymov <ilgyrd@gmail.com>
2019-03-24 20:37:28 +0200

    unit test corrected

--- test/test_helpers/create_encoder.dart
+++ test/test_helpers/create_encoder.dart
@@ -1,21 +1,17 @@
-import 'package:ml_preprocessing/src/categorical_encoder/category_values_extractor.dart';
-import 'package:ml_preprocessing/src/categorical_encoder/encode_unknown_strategy_type.dart';
 import 'package:ml_preprocessing/src/categorical_encoder/encoder.dart';
 import 'package:ml_preprocessing/src/categorical_encoder/encoder_type.dart';
 import 'package:ml_preprocessing/src/categorical_encoder/one_hot_encoder.dart';
 import 'package:ml_preprocessing/src/categorical_encoder/ordinal_encoder.dart';
 
 CategoricalDataEncoder createEncoder({
-  EncodeUnknownValueStrategy strategy,
-  CategoryValuesExtractor extractor,
-  List<Object> values,
+  List<String> values,
   CategoricalDataEncoderType type = CategoricalDataEncoderType.oneHot,
 }) {
   switch (type) {
     case CategoricalDataEncoderType.oneHot:
-      return OneHotEncoder(encodeUnknownValueStrategy: strategy, valuesExtractor: extractor)..setCategoryValues(values);
+      return OneHotEncoder();
     case CategoricalDataEncoderType.ordinal:
-      return OrdinalEncoder(encodeUnknownValueStrategy: strategy, valuesExtractor: extractor)..setCategoryValues(values);
+      return OrdinalEncoder();
     default:
       throw Error();
   }
--- test/test_helpers/test_csv_data.dart
+++ test/test_helpers/test_csv_data.dart
@@ -1,40 +1,40 @@
 import 'dart:async';
-import 'dart:typed_data';
 
-import 'package:ml_linalg/matrix.dart';
-import 'package:ml_linalg/vector.dart';
 import 'package:ml_preprocessing/src/categorical_encoder/encoder_factory.dart';
-import 'package:ml_preprocessing/src/ml_data/float32x4_csv_ml_data.dart';
-import 'package:ml_preprocessing/src/ml_data/validator/ml_data_params_validator.dart';
+import 'package:ml_preprocessing/src/data_frame/csv_data_frame.dart';
+import 'package:ml_preprocessing/src/data_frame/validator/params_validator.dart';
+import 'package:ml_linalg/matrix.dart';
 import 'package:test/test.dart';
 import 'package:tuple/tuple.dart';
 
 import '../mocks.dart';
 
-Future testCsvData({
-  String fileName,
-  int labelIdx,
-  int expectedColsNum,
-  int expectedRowsNum,
-  List<Tuple2<int, int>> rows,
-  List<Tuple2<int, int>> columns,
-  CategoricalDataEncoderFactory categoricalDataFactoryMock,
-  MLDataParamsValidator validatorMock,
-  void testContentFn(MLMatrix<Float32x4> features, MLVector<Float32x4> labels, List<String> headers)}) async {
-
+Future testCsvData(
+    {String fileName,
+    int labelIdx,
+    int expectedColsNum,
+    int expectedRowsNum,
+    List<Tuple2<int, int>> rows,
+    List<Tuple2<int, int>> columns,
+    CategoricalDataEncoderFactory categoricalDataFactoryMock,
+    DataFrameParamsValidator validatorMock,
+    void testContentFn(
+        Matrix features, Matrix labels, List<String> headers)}) async {
   categoricalDataFactoryMock ??= createCategoricalDataEncoderFactoryMock();
-  validatorMock ??= createMLDataParamsValidatorMock(validationShouldBeFailed: false);
+  validatorMock ??=
+      createDataFrameParamsValidatorMock(validationShouldBeFailed: false);
 
-  final data = Float32x4CsvMLDataInternal.fromFile(fileName,
+  final dataFrame = CsvDataFrame.fromFile(
+    fileName,
     labelIdx: labelIdx,
     columns: columns,
     rows: rows,
     encoderFactory: categoricalDataFactoryMock,
     paramsValidator: validatorMock,
   );
-  final header = await data.header;
-  final features = await data.features;
-  final labels = await data.labels;
+  final header = await dataFrame.header;
+  final features = await dataFrame.features;
+  final labels = await dataFrame.labels;
 
   if (columns == null) {
     expect(header.length, equals(expectedColsNum + 1));
@@ -42,7 +42,7 @@ Future testCsvData({
   }
 
   expect(features.rowsNum, equals(expectedRowsNum));
-  expect(labels.length, equals(expectedRowsNum));
+  expect(labels.rowsNum, equals(expectedRowsNum));
 
   testContentFn(features, labels, header);
 }
