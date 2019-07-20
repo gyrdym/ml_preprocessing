c302cf14ba7a8789fcf397def6dff9ef7ceab180
Ilya Gyrdymov <ilgyrd@gmail.com>
2019-07-21 01:10:54 +0300

    Categorical data encoders unit tests fixed

--- test/categorical_encoder/ordinal_encoder_test.dart
+++ test/categorical_encoder/ordinal_encoder_test.dart
@@ -1,84 +1,48 @@
-import 'package:ml_preprocessing/src/categorical_encoder/ordinal_encoder.dart';
 import 'package:ml_linalg/matrix.dart';
+import 'package:ml_preprocessing/src/categorical_encoder/encoder_factory_impl.dart';
+import 'package:ml_preprocessing/src/categorical_encoder/encoder_type.dart';
 import 'package:test/test.dart';
 
 void main() {
   group('OrdinalEncoder', () {
-    test('should encode categorical data, ordered collection of non-repeatable '
-        'labels', () {
-      final encoder = OrdinalEncoder();
-      expect(encoder.encode(['group A', 'group B', 'group C', 'group D']),
+    final values = ['group C', 'group D', 'group A', 'group B', 'group C',
+      'group A', 'group D'];
+
+    final encoder = CategoricalDataCodecFactoryImpl()
+        .fromType(CategoricalDataEncoderType.ordinal, values);
+
+    test('should extract all unique categorical values and create a map with '
+        '`<label> - <encoded>` key pairs', () {
+      expect(encoder.encode(['group D', 'group A', 'group C', 'group D',
+        'group B']),
         equals([
-          [0],
           [1],
           [2],
+          [0],
+          [1],
           [3],
         ]),
       );
-    });
-
-    test('should encode categorical data, unordered collection of unrepeatable'
-        'labels', () {
-      final encoder = OrdinalEncoder();
-      expect(encoder.encode(['group B', 'group D', 'group A', 'group C']),
-        equals([
-          [0],
-          [1],
-          [2],
-          [3],
-        ]),
-      );
-    });
-
-    test('should encode categorical data, unordered collection of repeatable'
-        'labels', () {
-      final encoder = OrdinalEncoder();
-      expect(encoder.encode(['group B', 'group D', 'group B', 'group A',
-        'group C', 'group C', 'group A']),
-        equals([
-          [0],
-          [1],
-          [0],
-          [2],
-          [3],
-          [3],
-          [2],
-        ]),
-      );
-    });
-
-    test('should provide symmetrical encoding/decoding', () {
-      final encoder = OrdinalEncoder();
-      final encoded = encoder.encode(['group B', 'group D', 'group B', 'group A',
-        'group C', 'group C', 'group A']);
-      expect(encoder.decode(encoded), equals(['group B', 'group D', 'group B',
-        'group A', 'group C', 'group C', 'group A']));
     });
 
     test('should decode categorical data', () {
-      final encoder = OrdinalEncoder();
-      encoder.encode(['group B', 'group D', 'group B', 'group A',
-        'group C', 'group C', 'group A']);
       expect(encoder.decode(Matrix.fromList([
         [2.0],
         [3.0],
         [3.0],
-      ])), equals(['group A', 'group C', 'group C']));
+      ])), equals(['group A', 'group B', 'group B']));
+    });
+
+    test('should return empty list as encoded value collection if empty list '
+        'passed to `encode` method', () {
+      expect(encoder.encode([]), equals([]));
     });
 
     test('should throw an exception if one tries to decode nonexistent '
         'category value', () {
-      final encoder = OrdinalEncoder();
-      encoder.encode(['group B', 'group D', 'group B', 'group A',
-      'group C', 'group C', 'group A']);
       expect(() => encoder.decode(Matrix.fromList([
         [25.0],
       ])), throwsException);
-    });
-
-    test('should throw an error if an empty label list is passed', () {
-      final encoder = OrdinalEncoder();
-      expect(() => encoder.encode([]), throwsException);
     });
   });
 }
