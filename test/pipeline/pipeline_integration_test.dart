7f4175745804a6430e804261753358f6ad0069af
Ilya Gyrdymov <ilgyrd@gmail.com>
2019-08-27 00:51:30 +0300

    pipeline integration tests extended, encoder tests extended

--- test/pipeline/pipeline_integration_test.dart
+++ test/pipeline/pipeline_integration_test.dart
@@ -6,8 +6,8 @@ import 'package:test/test.dart';
 
 void main() {
   group('Pipeline', () {
-    test('should create a pipeline, steps of which may return dataframes of '
-        'different series length', () {
+    test('should allow steps, which return dataframes of different series '
+        'number', () {
       final fittingData = DataFrame([
         <dynamic>['first', 'second',     'third',      'fourth'],
         <dynamic>[  1,        'F',   'category_val_1',    10   ],
@@ -27,7 +27,7 @@ void main() {
 
       final result = pipeline.process(fittingData);
 
-      expect(result.rows, equals([
+      expect(result.toMatrix(), equals([
         [  0,  1, 0,  1, 0, 0,  1, 0, 0,  ],
         [  1,  1, 0,  0, 1, 0,  0, 1, 0,  ],
         [  2,  0, 1,  1, 0, 0,  1, 0, 0,  ],
@@ -35,6 +35,36 @@ void main() {
         [  4,  0, 1,  1, 0, 0,  1, 0, 0,  ],
         [  5,  0, 1,  1, 0, 0,  0, 0, 1,  ],
         [  6,  1, 0,  0, 0, 1,  1, 0, 0,  ],
+      ]));
+    });
+
+    test('should not rewrite previously encoded series', () {
+      final fittingData = DataFrame([
+        <dynamic>['first', 'second',     'third',      'fourth'],
+        <dynamic>[  1,        'F',   'category_val_1',    10   ],
+        <dynamic>[  10,       'F',   'category_val_2',    20   ],
+        <dynamic>[  11,       'M',   'category_val_1',    10   ],
+        <dynamic>[  21,       'F',   'category_val_2',    30   ],
+        <dynamic>[  44,       'M',   'category_val_1',    10   ],
+        <dynamic>[  43,       'M',   'category_val_1',    30   ],
+        <dynamic>[  55,       'F',   'category_val_3',    10   ],
+      ]);
+
+      final pipeline = Pipeline(fittingData, [
+        oneHotEncode(columns: [1, 2]),
+        labelsEncode(columns: [0, 1, 3]),
+      ]);
+
+      final result = pipeline.process(fittingData);
+
+      expect(result.rows, equals([
+        [  0,  1, 0,  1, 0, 0,  0,  ],
+        [  1,  1, 0,  0, 1, 0,  1,  ],
+        [  2,  0, 1,  1, 0, 0,  0,  ],
+        [  3,  1, 0,  0, 1, 0,  2,  ],
+        [  4,  0, 1,  1, 0, 0,  0,  ],
+        [  5,  0, 1,  1, 0, 0,  2,  ],
+        [  6,  1, 0,  0, 0, 1,  0,  ],
       ]));
     });
   });
