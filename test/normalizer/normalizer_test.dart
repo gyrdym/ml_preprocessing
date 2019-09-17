83f59922b360e09e7792a1a52603f5881934eec6
Ilya Gyrdymov <ilgyrd@gmail.com>
2019-09-17 20:15:24 +0300

    unit tests for normalize operator added, unite tests for Normalizer extended

--- test/normalizer/normalizer_test.dart
+++ test/normalizer/normalizer_test.dart
@@ -6,8 +6,8 @@ import 'package:test/test.dart';
 
 void main() {
   group('Normalizer', () {
-    test('should divide each row-vector by its norm and preserve the header of '
-        'the input dataframe', () {
+    test('should divide each row-vector by its euclidean norm and preserve '
+        'the header of the input dataframe', () {
       final header = ['first', 'second', 'third'];
       final data = Matrix.fromList([
         [10,  20,  30 ],
@@ -21,11 +21,33 @@ void main() {
 
       expect(transformed.header, equals(header));
       expect(transformed.toMatrix(), iterable2dAlmostEqualTo([
-        [0.26, 0.53, 0.80],
-        [0.45, 0.56, 0.68],
-        [0.64, 0.57, 0.50],
-        [0.60, 0.57, 0.54],
-      ], 1e-2));
+        [0.267, 0.534, 0.801],
+        [0.455, 0.569, 0.683],
+        [0.646, 0.574, 0.502],
+        [0.608, 0.576, 0.544],
+      ], 1e-3));
+    });
+
+    test('should divide each row-vector by its manhattan norm and preserve '
+        'the header of the input dataframe', () {
+      final header = ['first', 'second', 'third'];
+      final data = Matrix.fromList([
+        [10,  20,  30 ],
+        [40,  50,  60 ],
+        [90,  80,  70 ],
+        [190, 180, 170],
+      ]);
+      final input = DataFrame.fromMatrix(data, header: header);
+      final normalizer = Normalizer(Norm.manhattan);
+      final transformed = normalizer.process(input);
+
+      expect(transformed.header, equals(header));
+      expect(transformed.toMatrix(), iterable2dAlmostEqualTo([
+        [0.166, 0.333, 0.500],
+        [0.266, 0.333, 0.400],
+        [0.375, 0.333, 0.291],
+        [0.351, 0.333, 0.314],
+      ], 1e-3));
     });
   });
 }
